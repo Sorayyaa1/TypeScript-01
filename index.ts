d6b12afe79f1f19211ee4a2968703b45be690222
Soraya <sorayyaa1@gmail.com>
2025-10-04 23:46:08 +0330

    create first TypeScript file

---
+++ index.ts
@@ -0,0 +1,16 @@
+let count:number
+count=7678686;
+console.log(count)
+
+let lastName:string="Kianfar"
+console.log(lastName)
+
+function RRR(num1:number,num2:number):number{
+   return num1*num2
+}
+
+console.log(RRR(3,3))
+
+
+let itemsList:string[]=["pen","pencil","hghjg"];
+console.log(itemsList)
